5c8bfb399d4eed27278e3444baacb987fbec481d
Andrew Burnett-Thompson <andrew@scichart.com>
2018-09-14 18:57:22 +0100

    Fixes startup error in Test App

--- SciChart.Wpf.UI.TestApp/App.xaml.cs
+++ SciChart.Wpf.UI.TestApp/App.xaml.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using SciChart.UI.Bootstrap;
+using Unity;
 
-namespace SciChart.Wpf.UI.Controls.Preview
+namespace SciChart.Wpf.UI.TestApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private AbtBootstrapper _bootstrapper;
+
+        public App()
+        {
+            _bootstrapper = new AbtBootstrapper(new UnityContainer(), new AttributedTypeDiscoveryService(new AutoAssemblyDiscovery()));
+            _bootstrapper.Initialize();
+        }
     }
 }
