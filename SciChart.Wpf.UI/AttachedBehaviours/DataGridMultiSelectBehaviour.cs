c1aa67bfacfc13f7515237cd43436a03825a9d24
Andrew Burnett-Thompson <andrew@scichart.com>
2019-05-23 19:08:37 +0100

    Updated to use MahApps.metro 2.0 Preview, which allows SciChart.Wpf.UI to build for .net core 3

--- SciChart.Wpf.UI/AttachedBehaviours/DataGridMultiSelectBehaviour.cs
+++ SciChart.Wpf.UI/AttachedBehaviours/DataGridMultiSelectBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Interactivity;
+using Microsoft.Xaml.Behaviors;
 
 namespace SciChart.Wpf.UI.AttachedBehaviours
 {
