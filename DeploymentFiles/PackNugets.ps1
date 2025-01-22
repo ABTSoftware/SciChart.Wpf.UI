2e0722f299fa5b84754f8ea62f40a3c637b01d46
Demien B <demi.dev@scichart.com>
2025-01-22 15:54:23 +0200

    Updated scripts to fix broken build steps for SciChart.WPF.UI

--- DeploymentFiles/PackNugets.ps1
+++ DeploymentFiles/PackNugets.ps1
@@ -1,10 +1,18 @@
 param ([Parameter(Mandatory=$true)][string]$version)
 
+$currentDir = Get-Location
+$deployDir = "$PSScriptRoot"
 $slnDir = "$PSScriptRoot/.."
 $packagesDir = "$slnDir/Packages"
 
+# Switch to the Deployment folder
+Set-Location $deployDir
+
+# Import the SafeRemoveDir function
 . ./SafeRemoveDir.ps1
 
+# Clean the root packages directory
+Write-Host "Clearing folders..."
 SafeRemoveDir $packagesDir
 
 function PackNuget {
@@ -16,7 +24,13 @@ function PackNuget {
     dotnet pack -c:Release "$slnDir/$project/$project.csproj" "-p:NuspecFile=$slnDir/$nuspec.nuspec" "-p:NuspecProperties=`"version=$version`"" -o "$packagesDir" --no-build
 }
 
+Write-Host "Creating packages..."
+
 PackNuget "SciChart.UI.Bootstrap" "SciChart.UI.Bootstrap"
 PackNuget "SciChart.UI.Reactive" "SciChart.UI.Reactive"
 PackNuget "SciChart.Wpf.UI" "SciChart.Wpf.UI"
 PackNuget "SciChart.Wpf.UI.Transitionz" "SciChart.Wpf.UI.Transitionz"
+
+Write-Host "Success!"
+
+Set-Location $currentDir
